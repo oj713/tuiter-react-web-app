9df90198031c3b75431abc04ea5ea018d5480bc5
Omi Johnson <ojohnson@bigelow.org>
2023-07-20 01:11:03 -0400

    labs through 2.2.4

---
+++ src/labs/a3/java-script.js
@@ -0,0 +1,18 @@
+import VariablesAndConstants from "./variables-and-constants";
+import VariableTypes from "./variable-types";
+import BooleanVariables from "./boolean-variables";
+
+// currently at 2.2.5 Conditionals
+
+function JavaScript() {
+    console.log("Hello World!");
+    return(
+       <div>
+            <h1>JavaScript</h1>
+            <VariablesAndConstants/>
+            <VariableTypes/>
+            <BooleanVariables/>
+       </div>
+    );
+ }
+ export default JavaScript
