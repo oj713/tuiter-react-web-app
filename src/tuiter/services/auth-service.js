355ee5e2383b17888a7a45910c28729d1eae1000
Omi Johnson <ojohnson@bigelow.org>
2023-08-11 03:36:37 -0400

    added some console logs

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 const USERS_URL = `${SERVER_API_URL}/users`
 
+console.log("USERS_URL", USERS_URL)
+
 const api = axios.create({ withCredentials: true})
 
 export const login = async({username, password}) => {
