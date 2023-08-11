a32831f6f4d25a059b244d8b562ebaf7fc4985a8
Omi Johnson <ojohnson@bigelow.org>
2023-08-11 03:16:48 -0400

    fixed environment variable

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const SERVER_API_URL = process.env.REACT_APP_API_BASE
-console.log("SERVER_API_URL: ", SERVER_API_URL)
+const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 const USERS_URL = `${SERVER_API_URL}/users`
 
 const api = axios.create({ withCredentials: true})
